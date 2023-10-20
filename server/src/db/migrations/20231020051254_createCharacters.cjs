2a5bbf0fe0ae9a21cc9f810c94e5f9373ce361aa
RipNasty <det625@gmail.com>
2023-10-20 01:17:27 -0400

    character migration updated

--- server/src/db/migrations/20231020051254_createCharacters.cjs
+++ server/src/db/migrations/20231020051254_createCharacters.cjs
@@ -5,9 +5,19 @@
 /**
  * @param {Knex} knex
  */
-exports.up = async (knex) => {}
+exports.up = async (knex) => {
+    return knex.schema.createTable("characters", (table) => {
+        table.bigIncrements("id")
+        table.string("race").notNullable()
+        table.integer("level").notNullable()
+        table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
+        table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
+    })
+}
 
 /**
  * @param {Knex} knex
  */
-exports.down = (knex) => {}
+exports.down = (knex) => {
+    return knex.schema.dropTableIfExists("characters")
+}
