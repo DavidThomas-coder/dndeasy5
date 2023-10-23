76f5390ec12134e7fa9d3698a094ed039da5de91
RipNasty <det625@gmail.com>
2023-10-23 19:04:12 -0400

    form on homepage.  need to flesh out functions

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,13 @@
 import React from "react";
+import CharacterForm from "./CharacterForm";
 
 const HomePage = (props) => {
 
     return (
         <div>
             <h1>d&dEasy HomePage</h1>
+      
+            <CharacterForm />
         </div>
     )
 }
