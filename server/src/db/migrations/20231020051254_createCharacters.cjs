8a211b1491f7ef2c8ddae04e1f929b15a858c6e1
RipNasty <det625@gmail.com>
2023-10-27 16:34:31 -0400

    removed level from character migration

--- server/src/db/migrations/20231020051254_createCharacters.cjs
+++ server/src/db/migrations/20231020051254_createCharacters.cjs
@@ -9,7 +9,7 @@ exports.up = async (knex) => {
     return knex.schema.createTable("characters", (table) => {
         table.bigIncrements("id")
         table.string("race").notNullable()
-        table.integer("level").notNullable()
+        // table.integer("level").notNullable()
         table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
         table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
     })
